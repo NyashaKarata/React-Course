1dbac55a82c824ea01d0b7332aa769a1270b84b6
Nyasha Karata <nyashakarata35@gmail.com>
2023-01-27 12:44:24 +0200

    getting started

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+
+
+function Hello() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>This is my hello component</h1>
+          
+        </header>
+      </div>
+    );
+  }
+  
+  export default Hello;
+  
